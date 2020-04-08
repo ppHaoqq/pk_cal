dc5547fa6b673f589273b3336932861041257390
daiki <daiki.nitta888@gmail.com>
2020-04-08 18:58:38 +0900

    startappまだしてない

--- dm_cal.py
+++ dm_cal.py
@@ -1,21 +1,26 @@
-import pokemon_db_connect
+from pymongo import MongoClient
+import pymongo
 
-attacker = # ポケモンDBから種族値引っ張ってくる
-a = # attackerの(((race*2)+individual+(effort/2))*50/100+5)*personality
+client = MongoClient('localhost', 27017)
+db = client.pokemon
+collection = db.pokedb
+
+attacker = collection.find_one({'name': 'ドラパルト'})
+a = ((attacker['a'] * 2 + 31 + (252) / 4) * 50 / 100 + 5) * 1.0  # attackerの(((race*2)+individual+(effort/2))*50/100+5)*personality
 skill = # ポケモンDBから技情報引っ張てくる
-defenser = # DBから種族値引っ張ってくる
-h = # defenserの((race*2)+individual+(effort/2))*50/100+50+10
-b = # defenserの(((race*2)+individual+(effort/2))*50/100+5)*personality
+defender = collection.find_one({'name': 'ミミッキュ'})
+h = (defender['h'] * 2 + 31 + (252) / 4) * 50 / 100 + 50 + 10  # defenderの((race*2)+individual+(effort/2))*50/100+50+10
+b = ((defender['b'] * 2 + 31 + (252) / 4) * 50 / 100 + 5) * 1.0  # defenderの(((race*2)+individual+(effort/2))*50/100+5)*personality
 
-width = {s: 1, w: 0.75}# シングル1 or ダブル0.75
+width = [1, 0.75]# シングル1 or ダブル0.75
 weather = # 天候補正　晴れ　雨　砂
-critical　=  1.5
+critical = 1.5
 random_num = [0.85, 0.86, 0.87, 0.88, 0.89, 0.9, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99, 1]# 0.85~1.00 の16段階
-type = {match: 1.5, discord: 1}# 一致かどうか 1 1.5
-compatibility = # skillとdefenderの相性　0 0.25 0.5 1 2 4
+type = [1.5, 1]# 一致かどうか 1 1.5
+compatibility = [0, 0.25, 0.5, 1, 2, 4]# skillとdefenderの相性　
 burn =  0.5
 m = (wall * sniper * colorgogle * mof * mhalf * mfilter * exbelt * lifeorb * choiceitem * berry)
 
-orgin_dmg = ((22 * skill * a / b) / 50 + 2) * width{} * weather * critical * type * compatibility * burn * m
+origin_dmg = ((22* skill* a/ b) / 50+ 2)* width[0]* weather* critical* type[0]* compatibility[4]* burn* m
 
-dmg = [origin_dmg * x for x in random_num]
+dmg = [origin_dmg* x for x in random_num]
--- pokemon_db_connect.py
+++ pokemon_db_connect.py
@@ -1,2 +1,37 @@
 # 名前に反応してDBからデータを取得、クラスの形に突っ込む
 # mysqlかな
+#　以下練習
+#import mysql.connector as mydb
+
+#conn = mydb.connect(
+    #host='127.0.0.1',
+    #port='3306',
+    #user='root',
+    #password='w9478zqh',
+    #db='sample1')
+#conn.ping(reconnect=True)
+#cur = conn.cursor()
+
+#参考サイト：https://lets-hack.tech/programming/languages/python/beautifulsoup/
+from pymongo import MongoClient
+import pymongo
+
+
+client = MongoClient('localhost', 27017)
+db = client.pokemon
+collection = db.pokedb
+
+#for i in collection.find({"a": {'$gte': 100}}).sort('s', pymongo.DESCENDING).limit(5):
+    #print(i)
+
+
+
+attacker = collection.find_one({'name': 'ドラパルト'})
+a = ((attacker['a'] * 2 + 31 + (252) / 4) * 50 / 100 + 5) * 1.0  # attackerの(((race*2)+individual+(effort/2))*50/100+5)*personality
+
+defender = collection.find_one({'name': 'ミミッキュ'})
+h = (defender['h'] * 2 + 31 + (252) / 4) * 50 / 100 + 50 + 10  # defenderの((race*2)+individual+(effort/2))*50/100+50+10
+b = ((defender['b'] * 2 + 31 + (252) / 4) * 50 / 100 + 5) * 1.0  # defenderの(((race*2)+individual+(effort/2))*50/100+5)*personality
+
+print(attacker['name'], a)
+print(defender['name'], h, b)
